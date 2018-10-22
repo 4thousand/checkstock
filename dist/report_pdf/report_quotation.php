de561c54309eb5d9916710faee3384cff26be095
Thanachot Khongmueng <thanachot.nut@gmail.com>
2018-10-22 16:27:03 +0700

    phptest

--- dist/report_pdf/report_quotation.php
+++ dist/report_pdf/report_quotation.php
@@ -1,7 +1,9 @@
 <?php
 $data = json_decode($_POST['dataquotation'],  true);
 // print_r($data);
-echo $data['datenow_datepicker'];
+// echo $data['datenow_datepicker'];
+$countdatenow = count($data['datenow_datepicker']);
+echo $countdatenow ;
 
 $delivery = $data['delivery_date'];
 $date2 = explode('-', $delivery);
@@ -17,6 +19,11 @@ $month   = $data3[1];
 $day  = $data3[2];
 $due_date = $day.'/'.$month.'/'.$year;
 
+if($countdatenow == 10 ){
+  $convertdate = $data['datenow_datepicker']
+  $datenow_datepicker =  date( "d/m/Y", $convertdate);
+  echo $datenow_datepicker.'25';
+}else if($countdatenow >= 10 ){
 $beforecut = $data['datenow_datepicker'];
 $cutstring = substr($beforecut,0,10);   
 $data4 = explode('-',$cutstring);
@@ -25,7 +32,7 @@ $month   = $data4[1];
 $day  = $data4[2];
 $datenow_datepicker = $day.'/'.$month.'/'.$year;
 
-
+}
 // $data['doc_date'];
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
