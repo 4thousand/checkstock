e3be1d0ce2b90e82e51257668392423e57096e06
Ariya Indhabhandhu <ice_ass_whole@hotmail.com>
2018-11-06 13:03:04 +0700

    add internal and external function

--- src/App.vue
+++ src/App.vue
@@ -163,203 +163,217 @@
 </template>
 
 <script>
-  import Login from "./components/login";
-  if (localStorage.Datauser) {
-  var Datauser = JSON.parse(localStorage.Datauser)
+import Login from "./components/login";
+if (localStorage.Datauser) {
+  var Datauser = JSON.parse(localStorage.Datauser);
 }
-  export default {
-    name: "app",
-    components: {
-      Login
+export default {
+  name: "app",
+  components: {
+    Login
+  },
+  data: () => ({
+    menuVisible: false,
+    showSidepanel: false,
+    showNavigation: false,
+    name: "",
+    topicmenu: "หน้าหลัก",
+    role: "",
+    branchname: "",
+    pic_path: "",
+    icon: "",
+    sale_code: "",
+    company_name: "",
+    selectedEmployee: null,
+    employees: [
+      "Jim Halpert",
+      "Dwight Schrute",
+      "Michael Scott",
+      "Pam Beesly",
+      "Angela Martin",
+      "Kelly Kapoor",
+      "Ryan Howard",
+      "Kevin Malone",
+      "Creed Bratton",
+      "Oscar Nunez",
+      "Toby Flenderson",
+      "Stanley Hudson",
+      "Meredith Palmer",
+      "Phyllis Lapin-Vance"
+    ]
+  }),
+  created() {
+    this.changetopicmenu();
+  },
+  watch: {
+    $route: "changetopicmenu"
+  },
+  methods: {
+    changetopicmenu() {
+      //  alert(this.$route.fullPath.search("quotation"))
+      if (this.$route.fullPath.search("quotation") == 1) {
+        this.topicmenucolor = "green";
+        this.topicmenu = "ใบเสนอราคา";
+        this.icon = "bookmark";
+      }
+      if (this.$route.fullPath.search("newquotation") == 1) {
+        this.topicmenucolor = "green";
+        this.topicmenu = "สร้าง ใบเสนอราคา";
+        this.icon = "bookmark";
+      }
+      if (this.$route.fullPath.search("index") == 1) {
+        this.topicmenucolor = "#448aff";
+        this.topicmenu = "หน้าหลัก";
+        this.icon = "bookmarks";
+      }
+      if (this.$route.fullPath.search("setting") == 1) {
+        this.topicmenucolor = "#ff9100";
+        this.topicmenu = "Setting";
+        this.icon = "build";
+      }
+      if (this.$route.fullPath.search("newsale") == 1) {
+        this.topicmenucolor = "#795548";
+        this.topicmenu = "สร้าง ใบสั่งขาย";
+        this.icon = "bookmark_border";
+      }
+      if (this.$route.fullPath.search("deposit") == 1) {
+        this.topicmenucolor = "#f4c20d";
+        this.topicmenu = "ใบเงินมัดจำ";
+        this.icon = "assignment";
+      }
     },
-    data: () => ({
-      menuVisible: false,
-      showSidepanel: false,
-      showNavigation: false,
-      name:'',
-      topicmenu:'หน้าหลัก',
-      role:'',
-      branchname:'',
-      pic_path:'',
-      icon:'',
-      sale_code:'',
-      company_name:'',
-      selectedEmployee: null,
-      employees: [
-        'Jim Halpert',
-        'Dwight Schrute',
-        'Michael Scott',
-        'Pam Beesly',
-        'Angela Martin',
-        'Kelly Kapoor',
-        'Ryan Howard',
-        'Kevin Malone',
-        'Creed Bratton',
-        'Oscar Nunez',
-        'Toby Flenderson',
-        'Stanley Hudson',
-        'Meredith Palmer',
-        'Phyllis Lapin-Vance'
-      ]
-    }),
-    created (){
-      this.changetopicmenu()
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
     },
-    watch:{
-      $route: 'changetopicmenu'
+    goindex(val) {
+      // localStorage.iddocno = 0
+      this.showNavigation = false;
+
+      if (val == "/deposit") {
+        this.$router.push({ name: "deposit", params: { id: 0 } });
+        return;
+      }
+
+      if (val == "/sale") {
+        this.$router.push({ name: "newsale", params: { id: 0 } });
+        return;
+      }
+
+      if (val == "/quotation") {
+        // this.topicmenu = 'ใบเสนอราคา'
+        this.$router.push({ name: "newquo", params: { id: 0 } });
+        return;
+      }
+
+      this.$router.push(val);
     },
-    methods: {
-      changetopicmenu(){
-        //  alert(this.$route.fullPath.search("quotation"))
-        if(this.$route.fullPath.search("quotation") == 1){
-          this.topicmenucolor = 'green'
-          this.topicmenu = 'ใบเสนอราคา'
-           this.icon = 'bookmark'
-        }
-        if(this.$route.fullPath.search("newquotation") == 1){
-          this.topicmenucolor = 'green'
-          this.topicmenu = 'สร้าง ใบเสนอราคา'
-           this.icon = 'bookmark'
-        }
-         if(this.$route.fullPath.search("index") == 1){
-           this.topicmenucolor = '#448aff'
-          this.topicmenu = 'หน้าหลัก'
-          this.icon = 'bookmarks'
-        }
-        if(this.$route.fullPath.search("setting") == 1){
-          this.topicmenucolor = '#ff9100'
-          this.topicmenu = 'Setting'
-             this.icon = 'build'
-         }
-
-          if(this.$route.fullPath.search("newsale") == 1){
-          this.topicmenucolor = '#795548'
-          this.topicmenu = 'สร้าง ใบสั่งขาย'
-          this.icon = 'bookmark_border'
-         }
-      },
-      toggleMenu() {
-        this.menuVisible = !this.menuVisible;
-      },
-      goindex(val) {
-        // localStorage.iddocno = 0
-          this.showNavigation = false
-
-        if(val == '/deposit'){
-         this.$router.push({ name : 'deposit',params : { id: 0}})
-         return
-        }
-
-        if(val == '/sale'){
-         this.$router.push({ name : 'newsale',params : { id: 0}})
-         return
-        }
-       
-        if(val == '/quotation'){
-          // this.topicmenu = 'ใบเสนอราคา'
-          this.$router.push({ name : 'newquo',params : { id: 0}})
-          return
-        }
-       
-        this.$router.push(val);
-      },
-        username () {
-  		 	// console.log(localStor/age.Datauser)
-           var Datauser = JSON.parse(localStorage.Datauser)
-           this.name = Datauser.username
-           this.role = Datauser.rolename
-           this.branchname = Datauser.branch_name
-           this.pic_path = Datauser.pic_path
-           this.sale_code = Datauser.sale_code
-           this.company_name = Datauser.company_name
-          //  console.log(this.branchname)
-        // localStorage.rolename = Datauser.rolename
-  	}
-    },
-    mounted() {
- 
-    setInterval(function () {
-  		this.username()
-    }.bind(this),1000)
-
+    username() {
+      // console.log(localStor/age.Datauser)
+      var Datauser = JSON.parse(localStorage.Datauser);
+      this.name = Datauser.username;
+      this.role = Datauser.rolename;
+      this.branchname = Datauser.branch_name;
+      this.pic_path = Datauser.pic_path;
+      this.sale_code = Datauser.sale_code;
+      this.company_name = Datauser.company_name;
+      //  console.log(this.branchname)
+      // localStorage.rolename = Datauser.rolename
     }
-  };
+  },
+  mounted() {
+    setInterval(
+      function() {
+        this.username();
+      }.bind(this),
+      1000
+    );
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 .search {
-    max-width: 500px;
-  }
+  max-width: 500px;
+}
 .md-avatar img {
-      width: 108%;
-    height: 108%;
+  width: 108%;
+  height: 108%;
 }
 
-  span,
-  label,
-  input,
-  h1 {
-    font-family: "Kanit", sans-serif !important;
-  }
-  
-  .page-container {
-    min-height: 100vh;
-    overflow: hidden;
-    position: relative;
-    // border: 1px solid rgba(#000, 0.12);
-  }
-  
-  // Demo purposes only
-  .md-drawer {
-    width: 230px;
-    max-width: calc(100vw - 125px);
-  }
-  
-  .md-content {
-    padding: 16px;
-  }
-  
-  .slide-fade-enter-active {
-    transition: all 1s ease;
-  }
-  
-  .slide-fade-enter,
-  .slide-fade-leave-to {
-    transform: translateX(10px);
-    opacity: 0;
-  }
+span,
+label,
+input,
+h1 {
+  font-family: "Kanit", sans-serif !important;
+}
+
+.page-container {
+  min-height: 100vh;
+  overflow: hidden;
+  position: relative;
+  // border: 1px solid rgba(#000, 0.12);
+}
+
+// Demo purposes only
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+
+.md-content {
+  padding: 16px;
+}
+
+.slide-fade-enter-active {
+  transition: all 1s ease;
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
 </style>
 
 <style>
 .md-content {
-      height: calc(100vh - 50px);
-      overflow: auto;
-    }
-  .container {
-    background-color: white;
-    box-shadow: 0 15px 35px rgba(50, 50, 93, 0.03), 0 5px 15px rgba(0, 0, 0, 0.06);
-  }
-  
-  span,
-  label,
-  input,
-  h1,
-  .md-title,
-  .md-table-head-label,
-  .md-table-cell-container,
-  p,
-  strong,
-  .md-button-content,tr td,tr th {
-    font-family: "Kanit", sans-serif !important;
-  }
-  
-  * {
-    box-sizing: border-box;
-  }
+  height: calc(100vh - 50px);
+  overflow: auto;
+}
+.container {
+  background-color: white;
+  box-shadow: 0 15px 35px rgba(50, 50, 93, 0.03), 0 5px 15px rgba(0, 0, 0, 0.06);
+}
+
+span,
+label,
+input,
+h1,
+.md-title,
+.md-table-head-label,
+.md-table-cell-container,
+p,
+strong,
+.md-button-content,
+tr td,
+tr th {
+  font-family: "Kanit", sans-serif !important;
+}
+
+* {
+  box-sizing: border-box;
+}
 </style>
 
 <style lang="scss">
-  @import "~vue-material/dist/theme/engine";
-  @include md-register-theme( "default", ( primary: md-get-palette-color(blue, A200), / / The primary color of your application accent: md-get-palette-color(orange, A200), / / The accent or secondary color theme: dark / / This can be dark or light));
-  @import "~vue-material/dist/theme/all";
+@import "~vue-material/dist/theme/engine";
+@include md-register-theme(
+  "default",
+  (
+    primary: md-get-palette-color(blue, A200),
+    // The primary color of your application accent: md-get-palette-color(orange, A200),
+    // The accent or secondary color theme: dark / / This can be dark or light
+  )
+);
+@import "~vue-material/dist/theme/all";
 </style>
